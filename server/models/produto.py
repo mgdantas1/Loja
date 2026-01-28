cc5bbe43171ad58d2e402128ccb3ee9e02a211c8
Davi Francisco <franciscodavi327@gmail.com>
2026-01-28 13:47:07 -0300

    feat: atualizar modelo de produto para usar Enum no tipo

--- server/models/produto.py
+++ server/models/produto.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import Enum, String
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
@@ -7,7 +7,7 @@ class Produtos(Base):
 
     id:Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     titulo:Mapped[str] = mapped_column(String(30), nullable=False)
-    tipo:Mapped[str] = mapped_column(String(30), nullable=False)
+    tipo:Mapped[str] = mapped_column(Enum('perfume', 'hidratante'), nullable=False)
     status:Mapped[bool] = mapped_column(nullable=False)
     quantidade:Mapped[int] = mapped_column(nullable=False)
     preco:Mapped[float] = mapped_column(nullable=False)
