f91c737ae8080facd8414f8007c3b4c64feb99be
Cecilya <anacecilya17@gmail.com>
2026-02-02 21:21:13 -0300

    style: estiliza página de produtos com Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -52,15 +52,15 @@ export default function App() {
                         Adicionar produto
                     </button>
                 </nav>
-                <section className='flex flex-wrapjustify-center gap-6'>
+                <section className='flex flex-wrap justify-center gap-6 m-6'>
                     {lista.map(produto => (
-                        <div className='border-2 border-black rounded-xl p-3' >
-                            <h1>{produto.titulo}</h1>
+                        <div className='border-2 border-black rounded-xl p-4 min-w-80 max-w-100 flex-1 flex flex-col gap-2' >
+                            <h1 className='font-bold border-b-2 border-b-gray-400 pb-2 mb-4 text-xl'>{produto.titulo}</h1>
                             <p>Tipo: {produto.tipo}</p>
                             <p>Quantidade: {produto.quantidade}</p>
-                            <p>Preço: R$ {(produto.preco / 100)}
+                            <p>Preço:<strong> R$ {(produto.preco / 100)}</strong>
                             </p>
-                            <button className='primary-btn w-fit px-4' onClick={() => abrirEdicao(produto)}>
+                            <button className='primary-btn w-fit px-6 py-0.5 self-end' onClick={() => abrirEdicao(produto)}>
                                 Editar
                             </button>
                         </div>
